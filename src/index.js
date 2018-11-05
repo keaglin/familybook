7aa681ce4244b50d302e5f0f7f8fc7146b21e2ee
K. Eaglin <kceaglin@gmail.com>
2018-11-05 00:10:57 -0500

    setting up front end routing translation for gh-pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/familybook">
     <App />
   </Router>, document.getElementById('root'))
 registerServiceWorker()
