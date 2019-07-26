0acea58a99c78ef56bfdd557baeea808d078d6ba
K. Eaglin <kceaglin@gmail.com>
2019-07-26 11:20:32 -0400

    cleans up home page code

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,9 +34,6 @@ class Home extends Component {
                   <div className="control">
                     <button className="button is-primary is-rounded" type="submit">Search</button>
                   </div>
-                  {/* <div className="control">
-                    <button className="button is-primary is-rounded" type="submit">Search</button>
-                  </div> */}
                 </div>
               </div>
             </div>
@@ -51,51 +48,7 @@ class Home extends Component {
         </div>
       </section>
     )
-    // return (
-    //   <section className="vcenter-parent">
-    //     <div className="columns">
-    //       <div className="column is-10 is-offset-1">
-    //           <h1 className="title is-1">Johnson &amp; Jones Family Book</h1>
-    //           <hr />
-    //       </div>
-    //     </div>
-    //     <div className="columns">
-    //       <div className="column is-6 is-offset-3">
-    //         <div className="field">
-    //           <label className="label">Name</label>
-    //           <div className="control">
-    //             <input className="input is-rounded" type="text" placeholder="Text input" />
-    //           </div>
-    //           <p class="help">This is a help text</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="columns">
-    //       <div className="column">
-    //         <h3 className="subtitle is-3">Discover Your Roots</h3>
-    //       </div>
-    //     </div>
-    //   </section>
-    // )
   }
 }
-// {/* <div class="hero-body">
-//   <div class="container">
-//     <h1 class="title">
-//       Fullheight title
-//     </h1>
-//     <h2 class="subtitle">
-//       Fullheight subtitle
-//     </h2>
-//   </div>
-// </div> */}
-// {/* <div className="hero">
-//   <div class="">
-//     <div class="">
-//       <input id="searchTerm" type="text" />
-//       <label class="active" for="searchTerm">Who, What, Where, or When?</label>
-//     </div>
-//   </div>
-// </div> */}
 
 export default Home
